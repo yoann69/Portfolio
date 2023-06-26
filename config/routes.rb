1f41b992c513bbed9e56f96cb3a15b1c9bc4cacb
yoann morillo <yoann.morillo@hotmail.fr>
2023-06-26 12:41:13 +0200

    first commit work on home page design

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get '/portfolio', to: 'portfolio#index'
+end
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+# Defines the root path route ("/")
+# root "articles#index"
